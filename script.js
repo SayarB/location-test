339876544784f2006fd9d4d89c880380bb09f4bb
SayarB <sayarbhattacharyya03@gmail.com>
2022-08-16 21:46:22 +0530

    fix: added success handler

--- script.js
+++ script.js
@@ -11,13 +11,27 @@ function sendLocation() {
   }
 }
 async function onSuccess(position) {
+  console.log(
+    "lat : " +
+      position.coords.latitude +
+      "\nlong : " +
+      position.coords.longitude
+  );
   const res = await axios.get(
     `https://attendencegdsc.herokuapp.com/give_attendence/7003149748/${position.coords.latitude}/${position.coords.longitude}`
   );
   console.log(res.data);
-
-  message.innerHTML = res.data.error || res.data.message;
-  distance.innerHTML = parseFloat(res.data.distance).toFixed(2) + "m";
+  if (res.data.error) {
+    message.innerHTML = res.data.error || res.data.message;
+    distance.innerHTML = parseFloat(res.data.distance).toFixed(2) + "m";
+  } else {
+    message.innerHTML = "In range";
+    distance.innerHTML =
+      "lat : " +
+      position.coords.latitude +
+      "\nlong : " +
+      position.coords.longitude;
+  }
 }
 function onError(err) {
   console.log("error : ", err);
